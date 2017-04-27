abd179c1f41755e8b50bfa7986ea5a3a9bb83d5a
Emanuel  Guerrero <eg@tsl.io>
2017-04-27 01:28:51 -0400

    Added setup for data base and added route for getting all trucks.

--- Sources/FoodTruckAPI/FoodTruck.swift
+++ Sources/FoodTruckAPI/FoodTruck.swift
@@ -67,5 +67,82 @@ public class FoodTruck: FoodTruckAPI {
     }
     
     private func setupDB() {
+        let couchClient = CouchDBClient(connectionProperties: connectionProps)
+        couchClient.dbExists(dbName) { (exists, error) in
+            if exists {
+                Log.info("DB exists")
+            } else {
+                Log.error("DB does not exist")
+                couchClient.createDB(self.dbName, callback: { (db, error) in
+                    if db != nil {
+                        Log.info("DB created!")
+                        self.setupDbDesign(db: db!)
+                    } else {
+                        Log.error("Unable to create DB \(self.dbName): Error \(error!)")
+                    }
+                })
+            }
+        }
+    }
+    
+    private func setupDbDesign(db: Database) {
+        let design: [String: Any] = [
+            "_id": "_design/foodtruckdesign",
+            "views": [
+                "all_documents": [
+                    "map": "function(doc) { emit(doc._id, [doc._id, doc._rev]); }"
+                ],
+                "all_trucks": [
+                    "map": "function(doc) { if (doc.type == 'foodtruck') { emit(doc._id, [doc._id, doc.name, doc.foodtype, doc.avgcost, doc.latitude, doc.longitude]); } }"
+                ],
+                "total_trucks": [
+                    "map": "function(doc) { if (doc.type == 'foodtruck') { emit(doc._id, 1); } }",
+                    "reduce": "_count"
+                ]
+            ]
+        ]
+        db.createDesign(designName, document: JSON(design)) { (json, error) in
+            if error != nil {
+                Log.error("Failed to create design: \(error!)")
+            } else {
+                Log.info("Design created: \(json!)")
+            }
+        }
+    }
+    
+    public func getAllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void) {
+        let couchClient = CouchDBClient(connectionProperties: connectionProps)
+        let database = couchClient.database(dbName)
+        database.queryByView("all_trucks", ofDesign: designName, usingParameters: [.descending(true), .includeDocs(true)]) { (doc, error) in
+            if let doc = doc, error == nil {
+                do {
+                    let trucks = try self.parseTrucks(doc)
+                    completion(trucks, nil)
+                } catch {
+                    completion(nil, error)
+                }
+            } else {
+                completion(nil, error!)
+            }
+        }
+    }
+    
+    func parseTrucks(_ document: JSON) throws -> [FoodTruckItem] {
+        guard let rows = document["rows"].array else {
+            throw APICollectionError.ParseError
+        }
+        let trucks: [FoodTruckItem] = rows.flatMap {
+            let doc = $0["value"]
+            guard let id = doc[0].string,
+                  let name = doc[1].string,
+                  let foodType = doc[2].string,
+                  let avgCost = doc[3].float,
+                  let latitude = doc[4].float,
+                  let longitude = doc[5].float else {
+                    return nil
+            }
+            return FoodTruckItem(docId: id, name: name, foodType: foodType, avgCost: avgCost, latitude: latitude, longitude: longitude)
+        }
+        return trucks
     }
 }
--- Sources/FoodTruckAPI/FoodTruckAPI.swift
+++ Sources/FoodTruckAPI/FoodTruckAPI.swift
@@ -1,2 +1,6 @@
 public protocol FoodTruckAPI {
+    
+    // MARK: - Trucks
+    // Get all Food Trucks
+    func getAllTrucks(completion: @escaping ([FoodTruckItem]?, Error?) -> Void)
 }
