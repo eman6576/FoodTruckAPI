e6c1d5dc968cf0a07bcb17f63e0cb37fa80eceae
Emanuel  Guerrero <eg@tsl.io>
2017-04-27 03:54:27 -0400

    Added unit tests for adding and getting a truck.

--- Tests/FoodTruckAPITests/FoodTruckAPITests.swift
+++ Tests/FoodTruckAPITests/FoodTruckAPITests.swift
@@ -2,14 +2,40 @@ import XCTest
 @testable import FoodTruckAPI
 
 class FoodTruckAPITests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(FoodTruckAPI().text, "Hello, World!")
-    }
-
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testAddTruck", testAddAndGetTruck),
     ]
+    
+    var trucks: FoodTruck?
+    
+    override func setUp() {
+        trucks = FoodTruck()
+        super.setUp()
+    }
+    
+    // Add and get specific truck
+    func testAddAndGetTruck() {
+        guard let trucks = trucks else {
+            XCTFail()
+            return
+        }
+        let addExpectation = expectation(description: "Add truck item")
+        // First add new truck
+        trucks.addTruck(name: "testAdd", foodType: "testType", avgCost: 0, latitude: 0, longtitude: 0) { (addedTruck, error) in
+            guard error == nil else {
+                XCTFail()
+                return
+            }
+            if let addedTruck = addedTruck {
+                trucks.getTruck(docId: addedTruck.docId, completion: { (returnedTruck, error) in
+                    // Assert that the added truck equals the returned truck
+                    XCTAssertEqual(addedTruck, returnedTruck)
+                    addExpectation.fulfill()
+                })
+            }
+        }
+        waitForExpectations(timeout: 5) { (error) in
+            XCTAssertNil(error, "addTruck Timeout")
+        }
+    }
 }
